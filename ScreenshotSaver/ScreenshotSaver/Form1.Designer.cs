0cc57a8b1bff3668b4e73fcb5d0cfb45710e66c5
baalu06 <kumar.bala17@hotmail.com>
2019-03-16 22:16:29 +0530

    first commit

---
+++ ScreenshotSaver/ScreenshotSaver/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ScreenshotSaver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_DocName = new System.Windows.Forms.TextBox();
+            this.lbl_DocName = new System.Windows.Forms.Label();
+            this.lbl_Path = new System.Windows.Forms.Label();
+            this.txt_Path = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.Worker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // txt_DocName
+            // 
+            this.txt_DocName.Location = new System.Drawing.Point(273, 92);
+            this.txt_DocName.Name = "txt_DocName";
+            this.txt_DocName.Size = new System.Drawing.Size(325, 26);
+            this.txt_DocName.TabIndex = 0;
+            // 
+            // lbl_DocName
+            // 
+            this.lbl_DocName.AutoSize = true;
+            this.lbl_DocName.Location = new System.Drawing.Point(86, 95);
+            this.lbl_DocName.Name = "lbl_DocName";
+            this.lbl_DocName.Size = new System.Drawing.Size(125, 20);
+            this.lbl_DocName.TabIndex = 1;
+            this.lbl_DocName.Text = "DocumentName";
+            // 
+            // lbl_Path
+            // 
+            this.lbl_Path.AutoSize = true;
+            this.lbl_Path.Location = new System.Drawing.Point(169, 164);
+            this.lbl_Path.Name = "lbl_Path";
+            this.lbl_Path.Size = new System.Drawing.Size(42, 20);
+            this.lbl_Path.TabIndex = 2;
+            this.lbl_Path.Text = "Path";
+            // 
+            // txt_Path
+            // 
+            this.txt_Path.Location = new System.Drawing.Point(273, 158);
+            this.txt_Path.Name = "txt_Path";
+            this.txt_Path.Size = new System.Drawing.Size(290, 26);
+            this.txt_Path.TabIndex = 3;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(190, 238);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(134, 39);
+            this.btn_Start.TabIndex = 4;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(349, 238);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(134, 39);
+            this.btn_Stop.TabIndex = 5;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // Worker
+            // 
+            this.Worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Worker_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.txt_Path);
+            this.Controls.Add(this.lbl_Path);
+            this.Controls.Add(this.lbl_DocName);
+            this.Controls.Add(this.txt_DocName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_DocName;
+        private System.Windows.Forms.Label lbl_DocName;
+        private System.Windows.Forms.Label lbl_Path;
+        private System.Windows.Forms.TextBox txt_Path;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.ComponentModel.BackgroundWorker Worker;
+    }
+}
+
---
+++ ScreenshotSaver/ScreenshotSaver/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenshotSaver
+{
+    public partial class Form1 : Form
+    {
+        string fileSavePath;
+        string documentName;
+        int cnt = 1;
+        public Form1()
+        {
+            //Application.AddMessageFilter(m_filter);
+            //this.KeyUp+= new KeyEventHandler(key_press);
+            InitializeComponent();
+        }
+        BackgroundWorker bgw = new BackgroundWorker();
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            fileSavePath = txt_Path.Text;
+            documentName = txt_DocName.Text;
+            //bgw.DoWork += new DoWorkEventHandler(Worker_Test);
+            //Worker.RunWorkerAsync(2000);
+            KeyboardListener.s_KeyEventHandler += new EventHandler(KeyboardListener_s_KeyEventHandler);
+        }
+        public void ScreenCapture(string initialDirectory,string filename)
+        {
+            using (BackgroundWorker worker = new BackgroundWorker())
+            {
+                Thread.Sleep(0);
+                this.Refresh();
+                worker.DoWork += delegate (object sender, DoWorkEventArgs e) {
+                    BackgroundWorker wkr = sender as BackgroundWorker;
+                    Rectangle bounds = new Rectangle(Location, Size);
+                    Thread.Sleep(300);
+                    //Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
+                    Size screenSize = Screen.AllScreens.Select(s=>s.Bounds).Aggregate(Rectangle.Union).Size;
+                    Bitmap bitmap = new Bitmap(screenSize.Width,screenSize.Height);
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        //g.CopyFromScreen(Location, Point.Empty, bitmap.Size);
+                        g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, screenSize);
+                    }
+                    e.Result = bitmap;
+                };
+                worker.RunWorkerCompleted += delegate (object sender, RunWorkerCompletedEventArgs e) {
+                    if (e.Error != null)
+                    {
+                        Exception err = e.Error;
+                        while (err.InnerException != null)
+                        {
+                            err = err.InnerException;
+                        }
+                        MessageBox.Show(err.Message, "Screen Capture",
+                            MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    }
+                    else if (e.Cancelled == true)
+                    {
+                    }
+                    else if (e.Result != null)
+                    {
+                        if (e.Result is Bitmap)
+                        {
+                            Bitmap bitmap = (Bitmap)e.Result;
+                            string path = fileSavePath != "" ? fileSavePath : initialDirectory;
+                            bitmap.Save(path+ "/"+filename+".jpg");
+                        }
+                    }
+                };
+                worker.RunWorkerAsync();
+            }
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.Close();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+        }
+        private void KeyboardListener_s_KeyEventHandler(object sender, EventArgs e)
+        {
+            KeyboardListener.UniversalKeyEventArgs eventArgs = (KeyboardListener.UniversalKeyEventArgs)e;
+            
+            // 256 : key down    257 : key up
+            if (eventArgs.m_Msg == 257 && eventArgs.m_Key==44)
+            {
+                string fName = documentName != "" ? documentName : "image";
+                //ScreenCapture(fileSavePath);
+                ScreenCapture(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                    fName +Convert.ToString(cnt));
+                cnt++;
+            }
+
+        }
+    }
+}
